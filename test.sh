6a235fb646cea7db79ac438b9f6dd76adee7fca6
Samuel Hurtado <lnx.wifi@gmail.com>
2024-10-26 20:28:34 +0200

    New test

--- test.sh
+++ test.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-for i in {1..700}; do
-  ./client 31677 "Mensaje $i" &
+server=$(pgrep server)
+
+if [ -z "$server" ]; then
+  echo "Error: No se encontró el proceso 'server'. Asegúrate de que esté en ejecución."
+  exit 1
+fi
+
+echo "Probando Minitalk en modo secuencial"
+echo "PID del servidor: $server"
+
+# Pruebas secuenciales
+for i in {1..5}; do
+  ret=$(./client "$server" "Mensaje $i")
+  echo "Test $i: Retorno: $ret"
 done
 
+echo "Pulsa [Enter] para probar en modo asincrónico..."
+read
+
+# Pruebas asincrónicas
+for i in {1..25}; do
+  ret=$(./client "$server" "Mensaje $i" &)
+  echo "Test Asincrónico $i: Retorno: $ret"
+done
+
+# Esperar a que todos los procesos en segundo plano terminen
+wait
+echo "Pruebas completadas."
